8a4523ca9552e9fd3751fa898aecebc9580b60cf
Ethan Guymon <eg428@byu.edu>
2025-03-03 20:21:20 -0700

    Game Service testing

---
+++ server/src/test/java/service/GameServiceTest.java
@@ -0,0 +1,85 @@
+package service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import chess.ChessGame;
+import dataaccess.*;
+import dataaccess.DataAccessException;
+import dataaccess.localMemory.*;
+import model.*;
+import service.exceptions.*;
+
+class GameServiceTest {
+    private AuthDataAccess authDataAObject;
+    private GameDataAccess gameDataAObject;
+    private UserDataAccess userDataAObject;
+    private String authToken;
+    private GameService gameService;
+    private UserData user;
+
+
+    @BeforeEach
+    public void clearBeforeTests() throws DataAccessException, FaultyRequestException, NameAlreadyInUseException {
+        authDataAObject = new AuthDataStorage();
+        gameDataAObject = new GameDataStorage();
+        userDataAObject = new UserDataStorage();
+
+        new ClearService(userDataAObject, authDataAObject, gameDataAObject).clear();
+
+
+        user = new UserData("myUser", "poopypants", "fake@notreal.com");
+        authToken = new UserService(userDataAObject, authDataAObject).register(user).authToken();
+        gameService = new GameService(authDataAObject, gameDataAObject);
+    }
+
+    @Test
+    public void testValidListGames() throws DataAccessException, FaultyRequestException, NameAlreadyInUseException, UnauthorizedUserException {
+        List<GameData> expectedEmpty = new ArrayList<>();
+        assertEquals(expectedEmpty, List.of(gameService.listGames(authToken)));
+    }
+
+    @Test
+    public void testInvalidListGames(){
+        assertThrows(UnauthorizedUserException.class, () -> {gameService.listGames("beep_bop_boop");});
+    }
+
+    @Test
+    public void testValidAddGame() throws DataAccessException, UnauthorizedUserException, FaultyRequestException, NameAlreadyInUseException {
+        int newGameID = gameService.addGame(authToken, "correct game name");
+        int expectedGameID = 0;
+
+        assertEquals(expectedGameID, newGameID);
+        assertEquals(1, List.of(gameService.listGames(authToken)).size());
+    }
+
+    @Test
+    public void testInvalidAddGame(){
+        assertThrows(FaultyRequestException.class, () -> {gameService.addGame(authToken, null);});
+    }
+
+    @Test
+    public void testValidJoinGame() throws DataAccessException, UnauthorizedUserException, FaultyRequestException, NameAlreadyInUseException {
+        int id = gameService.addGame(authToken, "correct game name");
+        chess.ChessGame.TeamColor color = ChessGame.TeamColor.WHITE;
+
+        gameService.joinGame(authToken, id, color);
+
+        GameData game = gameDataAObject.getGameData(id);
+        assertEquals(user.username(), game.whiteUsername());
+        assertNull(game.blackUsername());
+    }
+
+    @Test
+    public void testInvalidJoinGame() throws DataAccessException, UnauthorizedUserException, FaultyRequestException, NameAlreadyInUseException {
+        int id = gameService.addGame(authToken, "correct game name");
+        chess.ChessGame.TeamColor color = ChessGame.TeamColor.WHITE;
+
+        gameService.joinGame(authToken, id, color);
+        assertThrows(NameAlreadyInUseException.class, () ->{gameService.joinGame(authToken, id, color);});
+        assertThrows(FaultyRequestException.class, () -> {gameService.joinGame(authToken, 1, color);});
+    }
+}
