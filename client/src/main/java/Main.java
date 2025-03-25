e3ac8d6a5e1d45d35935d2b65a2ab04b4b45bc2c
Ethan Guymon <eg428@byu.edu>
2025-03-25 02:45:21 -0600

    Main Repl

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,19 @@
 import chess.*;
+import ui.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class Main {
-    public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        System.out.println("♕ 240 Chess Client: ♕");
+
+        var serverUrl = "http://localhost:8080";
+
+        if (args.length == 1) {
+            serverUrl = args[0];
+        }
+
+        new Repl(serverUrl).run();
     }
 }
